3daf0990f26faa7f5129ecde65652770e910aa00
Vanio Anibal Macamo <macamovanioanibal@gmail.com>
2024-07-15 15:11:50 +0200

    #Improvment: Validações.

--- khodar/khodar-02-js-be-prepared/server/src/controllers/SubscriberController.ts
+++ khodar/khodar-02-js-be-prepared/server/src/controllers/SubscriberController.ts
@@ -1,11 +1,19 @@
 import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { generate6DigitsNumber } from '../utils/utils';
 import { db } from "../database";
+import { z } from 'zod'
 
 export class SubscriberController{
 
     async create(request: FastifyRequest, reply: FastifyReply){
-        const { phone, provinceId, districtId} = request.body
+
+        const subscriberSchema = z.object({
+            phone      : z.string().regex(/^8[2-7]\d{7}/),
+            provinceId : z.string(),
+            districtId : z.string()
+        })
+
+        const { phone, provinceId, districtId} = subscriberSchema.parse(request.body)
 
         //Verificar se o User existe
         const subscriberExists = await db.subscriber.findUnique({where: {phone: String(phone)} });
