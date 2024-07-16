1365d7e8844d37b547a2a711ade4ba9092fea51d
Vanio Anibal Macamo <macamovanioanibal@gmail.com>
2024-07-16 13:08:54 +0200

    #Improvment: Notification route

--- khodar/khodar-02-js-be-prepared/server/src/routes/index.ts
+++ khodar/khodar-02-js-be-prepared/server/src/routes/index.ts
@@ -2,9 +2,11 @@ import fastify, { FastifyInstance } from "fastify";
 import { SubscriberController } from "../controllers/SubscriberController";
 import { request } from "http";
 import { AuthController } from "../controllers/authController";
+import { NotificationController } from "../controllers/NotificationController";
 
 const subscriberController = new SubscriberController();
 const authController = new AuthController();
+const notificationController = new NotificationController();
 
 export async function routes(fastify: FastifyInstance){
 
@@ -18,6 +20,9 @@ export async function routes(fastify: FastifyInstance){
 
     fastify.post("/auth/subscribers/opt", (request, reply) => authController.authOtp(request, reply));
     fastify.post("/auth/subscribers", (request, reply) => authController.loginSubscriber(request, reply));
+
+    fastify.post("/notifications", (request, reply) => notificationController.create(request , reply));
+    fastify.get("/notifications/:phone", (request, reply) => notificationController.show(request , reply)); 
 
 
 }
